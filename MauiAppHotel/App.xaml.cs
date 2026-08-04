5a2e543e47bcb390fdf8babf20c830483f5b4e31
Rayan Allaki <rayanallaki78@gmail.com>
2026-08-04 14:28:57 -0300

    Fiz a parte da view inicial com um frame que vai possuir linhas e colunas para registrar quantidade de crianças e adultos

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui;
 
 namespace MauiAppHotel
 {
@@ -7,7 +8,7 @@ namespace MauiAppHotel
         public App()
         {
             InitializeComponent();
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new Views.ContratacaoHospedagem());
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
--- MauiAppHotel/MauiProgram.cs
+++ MauiAppHotel/MauiProgram.cs
@@ -13,6 +13,10 @@ namespace MauiAppHotel
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("GermaniaOne-Regular.ttf", "GermaniaOne");
+                    fonts.AddFont("BebasNeue-Regular.ttf", "BebasNeue");
+                    fonts.AddFont("GoogleSans-Italic-VariableFont_GRAD,opsz,wght.ttf", "Italic");
+                    fonts.AddFont("GoogleSans-VariableFont_GRAD,opsz,wght.ttf", "GoogleSans-VariableFont_GRAD");
                 });
 
 #if DEBUG
