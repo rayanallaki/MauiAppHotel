3b9a4634cd06f7241a8a54d82ce40c4b43bfd1b6
Rayan Allaki <rayanallaki78@gmail.com>
2026-08-13 11:30:34 -0300

    Criei a pasta Models com a classe Quarto dentro

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,10 +1,39 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MauiAppHotel.Models;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Maui;
 
 namespace MauiAppHotel
 {
     public partial class App : Application
     {
+
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            { 
+                Descricao = "Suíte Super Luxo",
+                ValorDiariaAdulto = 110.00,
+                ValorDiariaCrianca = 55.00
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Luxo",
+                ValorDiariaAdulto = 80.00,
+                ValorDiariaCrianca = 40.00
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Single",
+                ValorDiariaAdulto = 50.00,
+                ValorDiariaCrianca = 25.00
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Crise",
+                ValorDiariaAdulto = 25.00,
+                ValorDiariaCrianca = 12.50
+            }
+        };
         public App()
         {
             InitializeComponent();
--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -2,8 +2,24 @@ namespace MauiAppHotel.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
 {
+	App PropriedadesApp;
 	public ContratacaoHospedagem()
 	{
 		InitializeComponent();
-	}
+
+		PropriedadesApp = (App)Application.Current;
+
+		pck_quarto.ItemsSource = PropriedadesApp.lista_quartos;
+    }
+	private void Button_Clicked(object sender, EventArgs e)
+    {
+		try 
+		{
+			Navigation.PushAsync(new HospedagemContratada());
+		}
+		catch(Exception ex) 
+		{ 
+			DisplayAlert("ERRO!", ex.Message, "OK");
+        }
+    }
 }
